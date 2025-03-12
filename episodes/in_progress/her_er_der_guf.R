747f934fc3d88d9c910dac8c9faca65be4a35ce5
Christian B. Knudsen <cbk@kb.dk>
2025-03-12 09:36:48 +0100

    noter om gode steder at lade sig inspirere af.

---
+++ episodes/in_progress/her_er_der_guf.R
@@ -0,0 +1,42 @@
+git:
+  https://carpentries-incubator.github.io/git-novice-branch-pr/
+  https://carpentries-incubator.github.io/git-Rstudio-course/
+  https://carpentries-incubator.github.io/advanced-git/
+  https://carpentries-incubator.github.io/collaborative-git-and-github-lesson/
+  
+gis:
+  https://carpentries-incubator.github.io/r-geospatial-urban/
+  
+stat:
+  https://carpentries-incubator.github.io/statistical-inference-for-biology/
+  https://carpentries-incubator.github.io/r-survival-analysis/
+  https://carpentries-incubator.github.io/statistics-r-public-health/
+  https://carpentries-incubator.github.io/simple-linear-regression-public-health/
+  https://carpentries-incubator.github.io/multiple-linear-regression-public-health/
+  https://carpentries-incubator.github.io/logistic-regression-public-health/
+  https://carpentries-incubator.github.io/linear-mixed-models-public-health/
+  https://carpentries-incubator.github.io/high-dimensional-stats-r/
+  https://carpentries-incubator.github.io/high-dimensional-analysis-in-python/
+  https://carpentries-incubator.github.io/statistical-probabilistic-programming-r/
+  
+ML:
+  https://carpentries-incubator.github.io/machine-learning-novice-r/
+  
+  
+blandet:
+  https://carpentries-incubator.github.io/open-science-with-r/
+  https://carpentries-incubator.github.io/lesson-R-packaging/
+  https://carpentries-incubator.github.io/markdown-intro/
+  https://carpentries-incubator.github.io/R-archaeology-lesson/
+  https://carpentries-incubator.github.io/r-tidyverse-4-datasets/
+  https://carpentries-incubator.github.io/R-ecology-lesson-intermediate/
+  https://carpentries-incubator.github.io/data-organisation-for-better-analysis/
+  https://carpentries-incubator.github.io/R-help-reprexes/
+  
+esoterica:
+  https://carpentries-incubator.github.io/lc-litsearchr/
+  https://carpentries-incubator.github.io/r-for-artists/
+  
+vis:
+  https://carpentries-incubator.github.io/intro-data-viz/
+  https://carpentries-incubator.github.io/hermes_stat_inf_data_vis/
